3ee8a616ae23efc3de88533fb84c6a148f1cecb7
Dennis Ploeger <develop@dieploegers.de>
2021-05-04 14:39:03 +0200

    fix: Use lower case key vault ids

--- access_policy.tf
+++ access_policy.tf
@@ -2,7 +2,7 @@ resource "azurerm_key_vault_access_policy" "keyvault-access-policy-objectids" {
   count        = length(var.allowed_objectids)
   object_id    = element(var.allowed_objectids, count.index)
   tenant_id    = var.azure_tenant_id
-  key_vault_id = azurerm_key_vault.keyvault.id
+  key_vault_id = lower(azurerm_key_vault.keyvault.id)
 
   key_permissions = [
     "Get",
@@ -44,7 +44,7 @@ resource "azurerm_key_vault_access_policy" "keyvault-access-policy-objectid-apps
   object_id      = element(split(":", element(var.allowed_objectid_apps, count.index)), 0)
   application_id = element(split(":", element(var.allowed_objectid_apps, count.index)), 1)
   tenant_id      = var.azure_tenant_id
-  key_vault_id   = azurerm_key_vault.keyvault.id
+  key_vault_id = lower(azurerm_key_vault.keyvault.id)
 
   key_permissions = [
     "Get",
